cfbbbda5019908075cf7500cb79b95620808d311
Philipp Hancke <philipp.hancke@googlemail.com>
2025-07-27 20:16:59 -0700

    remove filereader because in 2025 we dont need this anymore

--- main.js
+++ main.js
@@ -1,24 +1,20 @@
 import {WebRTCInternalsDumpImporter} from './import.js';
 
 const container = document.getElementById('tables');
-document.getElementById('import').onchange = function(evt) {
+document.getElementById('import').onchange = async (evt) => {
     evt.target.disabled = 'disabled';
+    document.getElementById('useReferenceTime').disabled = true;
+
     const files = evt.target.files;
     const file = files[0];
-    const reader = new FileReader();
-    reader.onload = (function(file) {
-        return function(e) {
-            let result = e.target.result;
-            if (typeof result === 'object') {
-                result = pako.inflate(result, {to: 'string'});
-            }
-            window.importer = new WebRTCInternalsDumpImporter(container);
-            importer.process(result);
-        };
-    })(file);
+    let stream;
     if (file.type === 'application/gzip') {
-        reader.readAsArrayBuffer(files[0]);
+        stream = file.stream().pipeThrough(new DecompressionStream('gzip'));
     } else {
-        reader.readAsText(files[0]);
+        stream = file.stream();
     }
+    const blob = await (new Response(stream)).blob();
+    window.importer = new WebRTCInternalsDumpImporter(container);
+    importer.process(await blob.text());
 }
+
